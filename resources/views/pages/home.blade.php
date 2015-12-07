73e6e06ad3b6aca468c8db010f8b697672929893
wangxianzhe015 <xianzhe_wang@yahoo.com>
2015-12-07 16:29:47 +0900

    homepage update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.home')
 
 @section('content')
+    <script type="text/javascript" src = "{{asset('js/base/home.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/base/home.css')}}">
 	<div id = "welcome-title">Welcome to Wang Xianzhe's Homepage</div>
 @stop
